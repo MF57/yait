be7c7996eab9d667e6ab487d5ad44e4ab317c894
Łukasz Gurdek <lukasz.gurdek@node1.com>
2017-04-20 15:21:18 +0200

    Initial mobile app tab navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,95 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ListView } from 'react-native';
+import Expo from 'expo';
+import { FontAwesome } from '@expo/vector-icons';
+import { DrawerNavigator, DrawerView, TabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class Issues extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Issues',
+    tabBarIcon: ({ tintColor }) => (
+      <FontAwesome name="th-list" size={24} color={tintColor} />
+    ),
+  };
+   constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload. Really? Yes</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ListView
+             dataSource={this.state.dataSource}
+             renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+    );
+  }
+}
+
+class Tickets extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Tickets',
+    tabBarIcon: ({ tintColor }) => (
+      <FontAwesome name="ticket" size={24} color={tintColor} />
+
+    ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
+}
+
+class Settings extends React.Component {
+  static navigationOptions = {
+    tabBarLabel: 'Settings',
+    tabBarIcon: ({ tintColor }) => (
+      <FontAwesome name="cog" size={24} color={tintColor} />
+
+    ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  icon: {
+    width: 26,
+    height: 26,
+  },
+});
+
+export default TabNavigator({
+  Home: {
+    screen: Issues,
+  },
+  Notifications: {
+    screen: Tickets,
+  },
+  Settings: {
+    screen: Settings
+  }
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    activeTintColor: '#e91e63',
   },
 });
