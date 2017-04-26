e6e8bca2cca8fceb0e896b68279487a2b2c9933b
Łukasz Gurdek <lukasz.gurdek@node1.com>
2017-04-26 09:51:46 +0200

    Mobile login mock

--- mobile/App.js
+++ mobile/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, Image, ListView, StatusBar, TouchableHi
 import Expo from 'expo';
 import { FontAwesome } from '@expo/vector-icons';
 import { StackNavigator, TabNavigator } from 'react-navigation';
+import store from 'react-native-simple-store';
 
 const iconSize = 24;
 
@@ -145,12 +146,27 @@ class Settings extends React.Component {
   render() {
     return (
       <DefaultContainer>
+        <Button onPress={this.props.screenProps.logout} title="Log out" />
       </DefaultContainer>
     );
   }
 }
 
-export default TabNavigator({
+class LoginView extends React.Component {
+  render() {
+    return (
+      <DefaultContainer>
+        <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', padding: 80}}>
+          <View style={{padding: 20}}><FontAwesome name="user-o" size={100} color="#999" /></View>
+          <View style={{padding: 20}}><FontAwesome name="ticket" size={100} color="#999" /></View>
+        </View>
+        <Button onPress={this.props.login} title="Log in" />
+      </DefaultContainer>
+    );
+  }
+}
+
+const LoggedinNav = TabNavigator({
   Issues: {
     screen: IssuesTab,
   },
@@ -168,3 +184,44 @@ export default TabNavigator({
     activeTintColor: '#e91e63',
   },
 });
+
+export default class NavWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store
+      .get('loginToken')
+      .then(
+        (loginToken) => {
+          if(loginToken) {
+            this.setState({loggedIn: true})
+          } else {
+            this.setState({loggedIn: false})
+          }
+        });
+  }
+
+  saveNewToken = (token) => {
+    store.save('loginToken', token);
+    this.setState({loggedIn: !!token});
+  }
+
+  login = () => {
+    this.saveNewToken("test");
+  }
+
+  logout = () => {
+    this.saveNewToken(null);
+  }
+
+  render() {
+    if(this.state.loggedIn) {
+      return <LoggedinNav screenProps={{logout: this.logout}} />;
+    } else {
+      return <LoginView login={this.login} />;
+    }
+  }
+}
