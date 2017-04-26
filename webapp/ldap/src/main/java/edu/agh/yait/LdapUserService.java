8cc5d61eace678fdfe3557eed35674151d86ced9
Passarinho <passarinho4@gmail.com>
2017-04-26 10:36:54 +0200

    fix

--- webapp/ldap/src/main/java/edu/agh/yait/LdapUserService.java
+++ webapp/ldap/src/main/java/edu/agh/yait/LdapUserService.java
@@ -1,12 +1,13 @@
 package edu.agh.yait;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface LdapUserService {
 
     Optional<User> getUser(String login, String password);
-    List<User> getUsers(List<UserId> ids);
+    Map<UserId, Optional<User>> getUsers(List<UserId> ids);
     List<User> getUserByGroup(GroupId groups);
 
 
