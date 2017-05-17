460c221dfb329e274d5b47d99efa533078081094
Łukasz Gurdek <lukasz.gurdek@node1.com>
2017-05-17 11:17:50 +0200

    Implement some endpoints

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, ListView, StatusBar, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ListView, StatusBar, TouchableHighlight, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Expo from 'expo';
 import { FontAwesome } from '@expo/vector-icons';
 import { StackNavigator, TabNavigator } from 'react-navigation';
 import store from 'react-native-simple-store';
+import axios from 'axios';
 
 const iconSize = 24;
+axios.defaults.baseURL = 'http://yait.lagiewka.pl';
 
 const styles = StyleSheet.create({
   container: {
     marginTop: Expo.Constants.statusBarHeight,
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
   row: {
     flex: 1,
@@ -31,9 +36,9 @@ const styles = StyleSheet.create({
 class DefaultContainer extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAwareScrollView style={styles.container} extraScrollHeight={50}>
         {this.props.children}
-      </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
@@ -43,12 +48,32 @@ class IssueView extends React.Component {
       title: `${navigation.state.params.title}`,
   });
 
-  render() {
-    const { params } = this.props.navigation.state;
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      data: {}
+    };
+  }
 
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    axios.get('/issues/' + params.issueId)
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.log("error")
+      console.log(error)
+    });
+  }
+
+  render() {
     return (
       <View style={{padding: 10}}>
-        <Text>Test - {params.title}</Text>
+        <Text>Test - {this.state.data.description} </Text>
       </View>
     );
   }
@@ -59,32 +84,37 @@ class IssuesList extends React.Component {
     title: 'Issues'
   };
 
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  constructor(props) {
+    super(props);
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows([
-        {
-          title: 'Nie ma ciepłej wody na III piętrze',
-          votes: 321
-        },
-        {
-          title: 'Teścik',
-          votes: 123
-        }
-      ]),
+      dataSource: this.ds.cloneWithRows([]),
+      loading: true
     };
+  }
+
+  componentDidMount() {
+    axios.get('/issues')
+    .then((response) => {
+      this.setState({
+        dataSource: this.ds.cloneWithRows(response.data)
+      })
+    })
+    .catch((error) => {
+      console.log("error")
+      console.log(error)
+    });
   }
 
   render() {
     return (
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <TouchableHighlight onPress={() => this.props.navigation.navigate('Single', rowData)}>
+          renderRow={(rowData) => <TouchableHighlight onPress={() => this.props.navigation.navigate('Single', {issueId: rowData.id, title: rowData.title})}>
             <View style={styles.row}>
               <View style={{flex:5}}><Text>{rowData.title}</Text></View>
               <View style={styles.votes}>
-                <Text style={{textAlign: 'right'}}>{rowData.votes}</Text>
+                <Text style={{textAlign: 'right'}}>{rowData.score}</Text>
               </View>
             </View>
           </TouchableHighlight>}
@@ -153,14 +183,61 @@ class Settings extends React.Component {
 }
 
 class LoginView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: 'tester',
+      password: 'password123',
+      wrongCredentials: false
+    };
+  }
+
+  focusNextField = (nextField) => {
+    this.refs[nextField].focus();
+  };
+
+  login = () => {
+    axios.post('/login', {
+      user: this.state.login, // TODO
+      password: this.state.password
+    })
+    .then((response) => {
+      this.props.saveNewToken(response.data.authenticationToken);
+    })
+    .catch((error) => {
+      this.setState({
+        wrongCredentials: true,
+        password: ''
+      })
+    });
+  }
+
   render() {
     return (
       <DefaultContainer>
-        <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', padding: 80}}>
+        <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row', paddingTop: 80}}>
           <View style={{padding: 20}}><FontAwesome name="user-o" size={100} color="#999" /></View>
           <View style={{padding: 20}}><FontAwesome name="ticket" size={100} color="#999" /></View>
         </View>
-        <Button onPress={this.props.login} title="Log in" />
+          {this.state.wrongCredentials && <Text> Wrong credentials. Please try again </Text>}
+          <TextInput
+            ref="1"
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(login) => this.setState({login})}
+            value={this.state.login}
+            returnKeyType="next"
+            onSubmitEditing={() => this.focusNextField('2')}
+            keyboardType="email-address"
+          />
+          <TextInput
+            ref="2"
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}
+            secureTextEntry={true}
+            returnKeyType="done"
+          />
+          <Button title="log in" onPress={this.login} />
       </DefaultContainer>
     );
   }
@@ -180,8 +257,7 @@ const LoggedinNav = TabNavigator({
   tabBarPosition: 'bottom',
   tabBarOptions: {
     showIcon: true,
-    showLabel: false,
-    activeTintColor: '#e91e63',
+    showLabel: false
   },
 });
 
@@ -197,6 +273,7 @@ export default class NavWrapper extends React.Component {
       .then(
         (loginToken) => {
           if(loginToken) {
+            axios.defaults.headers.common['Authorization'] = loginToken;
             this.setState({loggedIn: true})
           } else {
             this.setState({loggedIn: false})
@@ -206,11 +283,8 @@ export default class NavWrapper extends React.Component {
 
   saveNewToken = (token) => {
     store.save('loginToken', token);
+    axios.defaults.headers.common['Authorization'] = token;
     this.setState({loggedIn: !!token});
-  }
-
-  login = () => {
-    this.saveNewToken("test");
   }
 
   logout = () => {
@@ -221,7 +295,7 @@ export default class NavWrapper extends React.Component {
     if(this.state.loggedIn) {
       return <LoggedinNav screenProps={{logout: this.logout}} />;
     } else {
-      return <LoginView login={this.login} />;
+      return <LoginView saveNewToken={this.saveNewToken} />;
     }
   }
 }
