98915c28526fd1f1ef24e8fc25da63336014cf36
msendera <marcin.sendera@gmail.com>
2017-05-30 12:30:34 +0200

    Setting properties injection to MailerConfig

--- webapp/src/main/java/edu/agh/yait/mailer/MailerConfig.java
+++ webapp/src/main/java/edu/agh/yait/mailer/MailerConfig.java
@@ -5,9 +5,13 @@ package edu.agh.yait.mailer;
  */
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
@@ -16,24 +20,37 @@ import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 @SuppressWarnings("deprecation")
 @Configuration
 @ComponentScan(basePackages = "edu.agh.yait.mailer")
+@PropertySource("classpath:application.properties")
 public class MailerConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    private @Value("${mailer.host}") String host;
+    private @Value("${mailer.port}") int port;
+    private @Value("${mailer.username}") String username;
+    private @Value("${mailer.password}") String password;
+    private @Value("${mailer.mail.smtp.starttls.enable}") String mailSmtpStarttlsEnable;
+    private @Value("${mailer.mail.smtp.auth}") String mailSmtpAuth;
+    private @Value("${mailer.mail.transport.protocol}") String mailTransportProtocol;
+    private @Value("${mailer.mail.debug}") String mailDebug;
 
     @Bean
     public JavaMailSender getMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-
-        //TODO do injection of properties
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("kompoyait@gmail.com");
-        mailSender.setPassword("KompoYait333");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties javaMailProperties = new Properties();
-        javaMailProperties.put("mail.smtp.starttls.enable", "true");
-        javaMailProperties.put("mail.smtp.auth", "true");
-        javaMailProperties.put("mail.transport.protocol", "smtp");
-        javaMailProperties.put("mail.debug", "true");
+        javaMailProperties.put("mail.smtp.starttls.enable", mailSmtpStarttlsEnable);
+        javaMailProperties.put("mail.smtp.auth", mailSmtpAuth);
+        javaMailProperties.put("mail.transport.protocol", mailTransportProtocol);
+        javaMailProperties.put("mail.debug", mailDebug);
 
         mailSender.setJavaMailProperties(javaMailProperties);
         return mailSender;
